ddb272dda137df1beb7a49005781668110f5feac
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-04-10 15:10:55 +0500

    Home Page Complete

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,27 @@
+﻿using k190359_A1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace k190359_A1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly ILogger<ProjectsController> _logger;
+
+        public ProjectsController(ILogger<ProjectsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace k190359_A1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
