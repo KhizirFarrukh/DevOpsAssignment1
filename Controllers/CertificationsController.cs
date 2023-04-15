ceeec34d5417f9b0288961a2ee1ba6efe97571c8
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-04-15 13:19:09 +0500

    Certifications added and title name fixed

---
+++ Controllers/CertificationsController.cs
@@ -0,0 +1,27 @@
+﻿using k190359_A1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace k190359_A1.Controllers
+{
+    public class CertificationsController : Controller
+    {
+        private readonly ILogger<CertificationsController> _logger;
+
+        public CertificationsController(ILogger<CertificationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
